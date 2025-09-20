55cfe36047d91ca2c811a7f5849323a19cd6c28f
R1ch1131 <cfyz1553@mail.ru>
2025-09-20 10:56:02 +0600

    [Feat] feat popup

--- src/widgets/viewTab/ui/ViewTab.tsx
+++ src/widgets/viewTab/ui/ViewTab.tsx
@@ -7,19 +7,15 @@ import { StatBox } from "~/shared/ui/StatBox";
 export const ViewTab = () => {
   return (
     <div className="relative text-white">
-      <p className="mb-4 text-lg font-bold">Создатели</p>
+      <p className="mb-4 text-lg font-bold">Описание</p>
 
       <div>
         <div className="flex justify-between gap-5">
-          <div className="flex flex-col w-1/3">
-            <CreatorRow role="Режиссер:" name="Дени Вильнёв" />
-            <CreatorRow role="Продюсер:" name="Мэри Пэрент, Кейл Борайтер" />
-            <CreatorRow role="Оператор:" name="Грег Фрейзер" />
-          </div>
-          <div className="flex flex-col w-1/3">
-            <CreatorRow role="Сценарист:" name="Джон Спейтс, Дени Вильнёв" />
-            <CreatorRow role="Композитор:" name="Ханс Циммер" />
-            <CreatorRow role="Монтаж:" name="Джо Уокер" />
+          <div className="flex w-2/3">
+            <p>Полномасштабная война за Арракис начинается. 
+              Пол Атрейд принимает свою судьбу и бросает вызов Империи. 
+              Объединившись с фрименами и взяв под контроль Великого Шелка, он ведёт их в эпическую битву против сил Императора и Харконненов.
+               Революция неизбежна.</p>
           </div>
           <div className="flex flex-col gap-4 w-1/3">
             <GlassBox title="Детали">
@@ -41,19 +37,9 @@ export const ViewTab = () => {
       <div className="absolute top-40">
         <p className="pb-4">Статистика</p>
         <div className="flex gap-5 2k:gap-4">
-          <div className="flex flex-col gap-5 2k:gap-4">
-            <StatBox
-              value="8.7"
-              label="Рейтинг IMDb"
-              color="text-lightorange"
-            />
-            <StatBox
-              value="95%"
-              label="Rotten Tomatoes"
-              color="text-green-500"
-            />
-          </div>
-          <div className="flex flex-col gap-5 2k:gap-4">
+          <div className="grid grid-cols-2 gap-5">
+            <StatBox value="8.7" label="Рейтинг IMDb" color="text-lightorange"/>
+            <StatBox value="95%" label="Rotten Tomatoes" color="text-green-500"/>
             <StatBox value="1.2к" label="Рецензий" color="text-purple-500" />
             <StatBox value="12.2к" label="В списке" color="text-red-500" />
           </div>
