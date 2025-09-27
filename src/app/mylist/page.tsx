3e42dc6c01c7ed2d40cb124d7883f84cceb05c73
R1ch1131 <cfyz1553@mail.ru>
2025-09-27 15:05:10 +0600

    [Fix] fix mylist

--- src/app/mylist/page.tsx
+++ src/app/mylist/page.tsx
@@ -3,7 +3,7 @@ import { UnauthorizedMyList } from "~/features/UnauthorizedMyList";
 
 export default function HomePage() {
   return (
-    <div className="flex justify-center pt-60 2k:pt-96 items-center h-full">
+    <div className="flex justify-center items-center h-[calc(100vh-200px)]">
       <UnauthorizedMyList />
     </div>
   );
