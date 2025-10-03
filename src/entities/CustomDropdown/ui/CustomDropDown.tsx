f63d56e0a60d5dc7ecb3bc9f49b3b9c750ff37de
misseri <nutado2005@gmail.com>
2025-10-03 19:49:06 +0600

    [Feat]filters-headlessui

---
+++ src/entities/CustomDropdown/ui/CustomDropDown.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+import { ChevronDown } from "lucide-react";
+import { type SetStateAction } from "react";
+
+interface Option {
+  label: string;
+  value: string;
+}
+
+interface CustomDropdownProps {
+  label: string;
+  options: Option[];
+  selected: Option;
+  setSelected: (option: SetStateAction<Option>) => void;
+}
+
+export const CustomDropdown: React.FC<CustomDropdownProps> = ({
+  label,
+  options,
+  selected,
+  setSelected,
+}) => {
+  return (
+    <div>
+      <label className="mb-3 block text-sm font-medium text-white">
+        {label}
+      </label>
+      <Menu>
+        <MenuButton className="flex w-full justify-between rounded-xl border border-white/20 bg-white/10 px-4 py-3 text-left text-white transition-all duration-200 hover:bg-white/20 focus:outline-none">
+          {selected.label}
+          <ChevronDown className="h-5 w-5" />
+        </MenuButton>
+        <MenuItems
+          transition
+          anchor="bottom"
+          className="bg-darkblue z-10 mt-1 w-73 origin-top rounded-xl shadow-md transition duration-200 ease-out data-[closed]:scale-95 data-[closed]:opacity-0"
+        >
+          {options.map((option) => (
+            <MenuItem key={option.value}>
+              {({ focus }) => (
+                <button
+                  className={`block w-full px-4 py-1 text-left text-sm ${
+                    focus ? "rounded-xl bg-blue-600 text-white" : ""
+                  }`}
+                  onClick={() => {
+                    setSelected(option);
+                  }}
+                >
+                  {option.label}
+                </button>
+              )}
+            </MenuItem>
+          ))}
+        </MenuItems>
+      </Menu>
+    </div>
+  );
+};
--- src/entities/Filters/ui/Filters.tsx
+++ src/entities/Filters/ui/Filters.tsx
@@ -1,125 +1,99 @@
 import React from "react";
 import { useState } from "react";
+import { CustomDropdown } from "~/entities/CustomDropdown";
 
 export const Filters: React.FC = () => {
-  const [sortBy, setSortBy] = useState("");
-  const [selectedGenre, setSelectedGenre] = useState("");
-  const [selectedYear, setSelectedYear] = useState("");
-  const [filterBy, setFilterBy] = useState("");
+  const [selectedSort, setSelectedSort] = useState({
+    label: "По популярности",
+    value: "popularity",
+  });
+  const [selectedGenre, setSelectedGenre] = useState({
+    label: "Все жанры",
+    value: "all",
+  });
+  const [selectedYear, setSelectedYear] = useState({
+    label: "Все годы",
+    value: "any",
+  });
+  const [selectedCategory, setSelectedCategory] = useState({
+    label: "Все фильмы",
+    value: "all",
+  });
+
+  const sortOptions = [
+    { label: "По популярности", value: "popularity" },
+    { label: "По рейтингу", value: "rating" },
+    { label: "По году", value: "recent" },
+    { label: "По названию", value: "title" },
+  ];
   const genreOptions = [
-    "Все жанры",
-    "Action",
-    "Adventure",
-    "Comedy",
-    "Drama",
-    "Horror",
-    "Sci-Fi",
-    "Fantasy",
-    "Romance",
-    "Thriller",
-    "Biography",
-    "History",
+    { label: "Все жанры", value: "all" },
+    { label: "Action", value: "action" },
+    { label: "Adventure", value: "adventure" },
+    { label: "Comedy", value: "comedy" },
+    { label: "Drama", value: "drama" },
+    { label: "Horror", value: "horror" },
+    { label: "Sci-Fi", value: "sci-fi" },
+    { label: "Fantasy", value: "fantasy" },
+    { label: "Romance", value: "romance" },
+    { label: "Thriller", value: "thriller" },
+    { label: "Biography", value: "biography" },
+    { label: "History", value: "history" },
   ];
   const yearOptions = [
-    "Все годы",
-    "2025",
-    "2024",
-    "2023",
-    "2022",
-    "2021",
-    "2020",
-    "2019",
-    "2018",
-    "2017",
-    "2016",
+    { label: "Все годы", value: "all" },
+    { label: "2025", value: "2025" },
+    { label: "2024", value: "2024" },
+    { label: "2023", value: "2023" },
+    { label: "2022", value: "2022" },
+    { label: "2021", value: "2021" },
+    { label: "2020", value: "2020" },
+    { label: "2019", value: "2019" },
+    { label: "2018", value: "2018" },
+    { label: "2017", value: "2017" },
+    { label: "2016", value: "2016" },
   ];
+  const categoryOptions = [
+    { label: "Все фильмы", value: "all" },
+    { label: "Новинки", value: "new" },
+    { label: "В тренде", value: "trending" },
+    { label: "Топ рейтинг", value: "top-rated" },
+  ];
+
   const clearFilters = () => {
-    setSortBy("");
-    setSelectedYear("");
-    setSelectedGenre("");
-    setFilterBy("");
+    setSelectedSort({ label: "По популярности", value: "popularity" });
+    setSelectedGenre({ label: "Все жанры", value: "all" });
+    setSelectedYear({ label: "Все годы", value: "all" });
+    setSelectedCategory({ label: "Все фильмы", value: "all" });
   };
+
   return (
     <>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-4">
-        <div className="flex flex-col">
-          <label className="mb-3 block text-sm font-medium text-white">
-            Сортировка
-          </label>
-          <select
-            value={sortBy}
-            onChange={(e) => setSortBy(e.target.value)}
-            className="w-full rounded-xl border border-white/20 bg-white/10 px-4 py-3 text-white transition-all duration-200 hover:bg-white/20 focus:border-amber-500 focus:outline-none"
-          >
-            <option value="popularity" className="bg-gray-800">
-              По популярности
-            </option>
-            <option value="rating" className="bg-gray-800">
-              По рейтингу
-            </option>
-            <option value="recent" className="bg-gray-800">
-              По году
-            </option>
-            <option value="title" className="bg-gray-800">
-              По названию
-            </option>
-          </select>
-        </div>
-        <div className="flex flex-col">
-          <label className="mb-3 block text-sm font-medium text-white">
-            Жанр
-          </label>
-          <select
-            value={selectedGenre}
-            onChange={(e) => setSelectedGenre(e.target.value)}
-            className="w-full rounded-xl border border-white/20 bg-white/10 px-4 py-3 text-white transition-all duration-200 hover:bg-white/20 focus:border-amber-500 focus:outline-none"
-          >
-            {genreOptions.map((genre) => (
-              <option key={genre} value={genre} className="bg-gray-800">
-                {genre}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="flex flex-col">
-          <label className="mb-3 block text-sm font-medium text-white">
-            Год выпуска
-          </label>
-          <select
-            value={selectedYear}
-            onChange={(e) => setSelectedYear(e.target.value)}
-            className="w-full rounded-xl border border-white/20 bg-white/10 px-4 py-3 text-white transition-all duration-200 hover:bg-white/20 focus:border-amber-500 focus:outline-none"
-          >
-            {yearOptions.map((year) => (
-              <option key={year} value={year} className="bg-gray-800">
-                {year}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="flex flex-col">
-          <label className="mb-3 block text-sm font-medium text-white">
-            Категория
-          </label>
-          <select
-            value={filterBy}
-            onChange={(e) => setFilterBy(e.target.value)}
-            className="w-full rounded-xl border border-white/20 bg-white/10 px-4 py-3 text-white transition-all duration-200 hover:bg-white/20 focus:border-amber-500 focus:outline-none"
-          >
-            <option value="all" className="bg-gray-800">
-              Все фильмы
-            </option>
-            <option value="new" className="bg-gray-800">
-              Новинки
-            </option>
-            <option value="trending" className="bg-gray-800">
-              В тренде
-            </option>
-            <option value="top-rated" className="bg-gray-800">
-              Топ рейтинг
-            </option>
-          </select>
-        </div>
+        <CustomDropdown
+          label="Сортировка"
+          options={sortOptions}
+          selected={selectedSort}
+          setSelected={setSelectedSort}
+        />
+        <CustomDropdown
+          label="Жанр"
+          options={genreOptions}
+          selected={selectedGenre}
+          setSelected={setSelectedGenre}
+        />
+        <CustomDropdown
+          label="Год выпуска"
+          options={yearOptions}
+          selected={selectedYear}
+          setSelected={setSelectedYear}
+        />
+        <CustomDropdown
+          label="Категория"
+          options={categoryOptions}
+          selected={selectedCategory}
+          setSelected={setSelectedCategory}
+        />
       </div>
       <div className="mt-6 flex w-full items-center justify-between">
         <div className="flex items-center gap-2">
